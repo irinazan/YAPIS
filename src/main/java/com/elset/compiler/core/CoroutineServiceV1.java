fba729ff2b34eb29039162044a03d1fb1c42609d
irinazan <57196078+irinazan@users.noreply.github.com>
2022-05-01 13:30:49 +0300

    individual task

---
+++ src/main/java/com/elset/compiler/core/CoroutineServiceV1.java
@@ -0,0 +1,12 @@
+package com.elset.compiler.core;
+
+import com.elset.compiler.gen.CoroutineService;
+
+public class CoroutineServiceV1 extends CoroutineService {
+
+
+    public CoroutineServiceV1(CoroutineContext rContext) {
+        super(rContext);
+    }
+
+}
